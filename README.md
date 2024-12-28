1f74167d79bd8d291b78e9a8141965c20060b9f1
zz <35785077@qq.com>
2024-12-28 18:25:33 +0800

    conf

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # c51
-代码来源：https://jiangxiekeji.com/
+代码来源：https://jiangxiekeji.com/  
+
+工程选项-Debug，下拉列表选择对应调试器，调试器Settings-Flash Download里勾选Reset and Run
--- z-test/User/main.c
+++ z-test/User/main.c
@@ -1,5 +1,46 @@
 #include "stm32f10x.h"                  // Device header
 
+/**
+  * @brief  微秒级延时
+  * @param  xus 延时时长，范围：0~233015
+  * @retval 无
+  */
+void Delay_us(uint32_t xus)
+{
+	SysTick->LOAD = 72 * xus;				//设置定时器重装值
+	SysTick->VAL = 0x00;					//清空当前计数值
+	SysTick->CTRL = 0x00000005;				//设置时钟源为HCLK，启动定时器
+	while(!(SysTick->CTRL & 0x00010000));	//等待计数到0
+	SysTick->CTRL = 0x00000004;				//关闭定时器
+}
+
+/**
+  * @brief  毫秒级延时
+  * @param  xms 延时时长，范围：0~4294967295
+  * @retval 无
+  */
+void Delay_ms(uint32_t xms)
+{
+	while(xms--)
+	{
+		Delay_us(1000);
+	}
+}
+ 
+/**
+  * @brief  秒级延时
+  * @param  xs 延时时长，范围：0~4294967295
+  * @retval 无
+  */
+void Delay_s(uint32_t xs)
+{
+	while(xs--)
+	{
+		Delay_ms(1000);
+	}
+} 
+
+
 int main(void)
 {
 //	RCC->APB2ENR = 0x00000010;
@@ -15,6 +56,9 @@ int main(void)
 GPIO_ResetBits(GPIOC, GPIO_Pin_13);
 	while(1)
 	{
-		
+		GPIO_SetBits(GPIOC, GPIO_Pin_13);
+		Delay_ms(500);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+		Delay_ms(500);
 	}
 }
